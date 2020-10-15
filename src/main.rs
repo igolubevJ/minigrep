502248d19659ea440d1b74d489008cba36870633
igolubev <igolubevapp@gmail.com>
2020-10-15 13:05:53 +0300

    Add feature env parsing and read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Поиск {}", query);
+    println!("В файле {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Что-то пошло не так при чтении файла.");
+
+    println!("С текстом:\n{}", contents);
 }
+
