bbae701e65e47317806c41e86f358a8a41df12ea
igolubev <igolubevapp@gmail.com>
2020-10-15 13:10:32 +0300

    Create parse_config feature

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Поиск {}", query);
     println!("В файле {}", filename);
@@ -14,5 +13,12 @@ fn main() {
         .expect("Что-то пошло не так при чтении файла.");
 
     println!("С текстом:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
 
