1f4a8519b9d703a2a26891ddb7ea19e3c1495493
igolubev <igolubevapp@gmail.com>
2020-10-15 13:52:04 +0300

    Create lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,35 +13,9 @@ fn main() {
     println!("Поиск {}", config.query);
     println!("В файле {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Ошибка в приложении: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("С текстом:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("недостаточно аргументов");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
